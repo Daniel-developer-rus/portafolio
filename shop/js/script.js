9b4d839e8008ca102a1e0ab63b1ca5b1db5126c7
Daniel-developer-rus <61740164+Daniel-developer-rus@users.noreply.github.com>
2020-06-10 03:05:48 +0400

    subi los files

---
+++ shop/js/script.js
@@ -0,0 +1,14 @@
+    $(document).ready(function(){
+	  $('.cowhide__slider').slick({
+	  dots:true,
+	  arrows:false,
+	  slidesToShow: 1,
+	  slidesToScroll: 1,
+	  autoplay: true,
+	  autoplaySpeed: 3000,
+	  });
+    });
+
+     $(document).ready(function(){
+	  $('.branded__slider').slick();
+	});
